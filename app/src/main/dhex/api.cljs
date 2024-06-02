9e16537b06accf0c1f4df531edb13991571ca3b6
dhaniel1 <Obioradaniel60@gmail.com>
2024-06-02 07:13:38 +0100

    Fix: includes api ns as cljs route util file

---
+++ app/src/main/dhex/api.cljs
@@ -0,0 +1,12 @@
+(ns dhex.api
+  (:require [clojure.string :as string :refer [join]]))
+
+(def api-url "https://api.realworld.io/api")
+(def localhost "http://localhost:4000")
+
+(defn endpoint
+  "concatinates api-url with / "
+ [& args]
+ (->> (cons localhost args) 
+     (join "/")))
+
